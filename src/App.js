baedf4c8c68238cb05281125a83b3f85ca705176
Narong <nnarongpen@gmail.com>
2020-08-31 14:37:32 +1000

    this.state add and render jsx

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'as1a'
+        },
+        {
+          name: 'Dracular',
+          id: 'as2a'
+        },
+        {
+          name: 'Zombie',
+          id: 'as3a'
+        }
+      ]
+    };
+  }
+
+
   render() {
     return (
       <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Narong.
-      </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-      </a>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
       </div>
     )
   }
