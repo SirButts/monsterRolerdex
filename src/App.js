2407dfb27dd3c051bf7b97cf0e61cfb0844d93af
Narong <nnarongpen@gmail.com>
2020-09-01 15:15:40 +1000

    add jsonplaceholder api, used promise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'as1a'
-        },
-        {
-          name: 'Dracular',
-          id: 'as2a'
-        },
-        {
-          name: 'Zombie',
-          id: 'as3a'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
 
